5b98e2c710b8a622da8203be696567aafd5c075b
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-02-26 12:19:06 +0400

    feat: updates repo

--- scripts/pack-all.sh
+++ scripts/pack-all.sh
@@ -6,14 +6,13 @@ OUTPUT_DIR="build/Packages"
 rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 echo "🚀 Packing all workspaces..."
-workspaces=$(yarn workspaces list --json | jq -r 'select(.location != ".") | .name')
+workspaces=$(jq -r '.workspaces[]' package.json)
 
 for workspace in $workspaces; do
     echo "📦 Packing $workspace..."
-    cd "$(yarn workspace "$workspace" exec pwd)" || exit
+    cd "$SCRIPT_DIR/$workspace" || exit
     filename=$(echo "$workspace" | sed 's/\//-/g; s/@//g')
-    yarn workspace "$workspace" pack --filename "$filename.tgz"
-    mv "$filename.tgz" "$SCRIPT_DIR/$OUTPUT_DIR/"
+    npm pack --pack-destination "$SCRIPT_DIR/$OUTPUT_DIR/"
 done
 
 echo "✅ All packages packed in $OUTPUT_DIR/"
