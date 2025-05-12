644bef661fd6ed1c8d343e4957272cae262f9a28
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-05-12 16:33:15 +0400

    refactor: update

--- packages/tx/src/capabilities/eip2718.ts
+++ packages/tx/src/capabilities/eip2718.ts
@@ -1,30 +1,30 @@
-import { RLP } from "@theqrl/zondjs-rlp";
-import { BIGINT_0, BIGINT_1, concatBytes } from "@theqrl/zondjs-util";
-import { keccak256 } from "ethereum-cryptography/keccak.js";
+// import { RLP } from "@theqrl/zondjs-rlp";
+// import { BIGINT_0, BIGINT_1, concatBytes } from "@theqrl/zondjs-util";
+// import { keccak256 } from "ethereum-cryptography/keccak.js";
 
-import { txTypeBytes } from "../util.js";
+// import { txTypeBytes } from "../util.js";
 
-import { errorMsg } from "./legacy.js";
+// import { errorMsg } from "./legacy.js";
 
-import type { EIP2718CompatibleTx } from "../types.js";
-import type { Input } from "@theqrl/zondjs-rlp";
+// import type { EIP2718CompatibleTx } from "../types.js";
+// import type { Input } from "@theqrl/zondjs-rlp";
 
-export function getHashedMessageToSign(tx: EIP2718CompatibleTx): Uint8Array {
-  const keccakFunction = tx.common.customCrypto.keccak256 ?? keccak256;
-  return keccakFunction(tx.getMessageToSign());
-}
+// export function getHashedMessageToSign(tx: EIP2718CompatibleTx): Uint8Array {
+//   const keccakFunction = tx.common.customCrypto.keccak256 ?? keccak256;
+//   return keccakFunction(tx.getMessageToSign());
+// }
 
-export function serialize(tx: EIP2718CompatibleTx, base?: Input): Uint8Array {
-  return concatBytes(txTypeBytes(tx.type), RLP.encode(base ?? tx.raw()));
-}
+// export function serialize(tx: EIP2718CompatibleTx, base?: Input): Uint8Array {
+//   return concatBytes(txTypeBytes(tx.type), RLP.encode(base ?? tx.raw()));
+// }
 
-export function validateYParity(tx: EIP2718CompatibleTx) {
-  const { v } = tx;
-  if (v !== undefined && v !== BIGINT_0 && v !== BIGINT_1) {
-    const msg = errorMsg(
-      tx,
-      "The y-parity of the transaction should either be 0 or 1",
-    );
-    throw new Error(msg);
-  }
-}
+// export function validateYParity(tx: EIP2718CompatibleTx) {
+//   const { v } = tx;
+//   if (v !== undefined && v !== BIGINT_0 && v !== BIGINT_1) {
+//     const msg = errorMsg(
+//       tx,
+//       "The y-parity of the transaction should either be 0 or 1",
+//     );
+//     throw new Error(msg);
+//   }
+// }
