3cd443dca8c2b52ee5211e292b7e43a83e0b3136
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-05-15 15:14:52 +0400

    refactor: update

--- packages/tx/src/legacy/constructors.ts
+++ packages/tx/src/legacy/constructors.ts
@@ -1,10 +1,10 @@
-import { RLP } from "@theqrl/zondjs-rlp";
-import { validateNoLeadingZeroes } from "@theqrl/zondjs-util";
+import { RLP } from '@theqrl/zondjs-rlp'
+import { validateNoLeadingZeroes } from '@theqrl/zondjs-util'
 
-import { LegacyTx } from "./tx.js";
+import { LegacyTx } from './tx.js'
 
-import type { TxOptions } from "../types.js";
-import type { TxData, TxValuesArray } from "./tx.js";
+import type { TxOptions } from '../types.js'
+import type { TxData, TxValuesArray } from './tx.js'
 
 /**
  * Instantiate a transaction from a data dictionary.
@@ -15,7 +15,7 @@ import type { TxData, TxValuesArray } from "./tx.js";
  * - All parameters are optional and have some basic default values
  */
 export function createLegacyTx(txData: TxData, opts: TxOptions = {}) {
-  return new LegacyTx(txData, opts);
+  return new LegacyTx(txData, opts)
 }
 
 /**
@@ -23,21 +23,25 @@ export function createLegacyTx(txData: TxData, opts: TxOptions = {}) {
  *
  * Format: `[nonce, gasPrice, gasLimit, to, value, data, v, r, s]`
  */
-export function createLegacyTxFromBytesArray(
-  values: TxValuesArray,
-  opts: TxOptions = {},
-) {
+export function createLegacyTxFromBytesArray(values: TxValuesArray, opts: TxOptions = {}) {
   // If length is not 6, it has length 9. If v/r/s are empty Uint8Arrays, it is still an unsigned transaction
   // This happens if you get the RLP data from `raw()`
   if (values.length !== 6 && values.length !== 9) {
     throw new Error(
-      "Invalid transaction. Only expecting 6 values (for unsigned tx) or 9 values (for signed tx).",
-    );
+      'Invalid transaction. Only expecting 6 values (for unsigned tx) or 9 values (for signed tx).',
+    )
   }
 
-  const [nonce, gasPrice, gasLimit, to, value, data, v, r, s] = values;
+  const [nonce, gasPrice, gasLimit, to, value, data, publicKey, signature] = values
 
-  validateNoLeadingZeroes({ nonce, gasPrice, gasLimit, value, v, r, s });
+  validateNoLeadingZeroes({
+    nonce,
+    gasPrice,
+    gasLimit,
+    value,
+    publicKey,
+    signature,
+  })
 
   return new LegacyTx(
     {
@@ -47,12 +51,11 @@ export function createLegacyTxFromBytesArray(
       to,
       value,
       data,
-      v,
-      r,
-      s,
+      publicKey,
+      signature,
     },
     opts,
-  );
+  )
 }
 
 /**
@@ -61,15 +64,12 @@ export function createLegacyTxFromBytesArray(
  * Format: `rlp([nonce, gasPrice, gasLimit, to, value, data,
  * signatureV, signatureR, signatureS])`
  */
-export function createLegacyTxFromRLP(
-  serialized: Uint8Array,
-  opts: TxOptions = {},
-) {
-  const values = RLP.decode(serialized);
+export function createLegacyTxFromRLP(serialized: Uint8Array, opts: TxOptions = {}) {
+  const values = RLP.decode(serialized)
 
   if (!Array.isArray(values)) {
-    throw new Error("Invalid serialized tx input. Must be array");
+    throw new Error('Invalid serialized tx input. Must be array')
   }
 
-  return createLegacyTxFromBytesArray(values as TxValuesArray, opts);
+  return createLegacyTxFromBytesArray(values as TxValuesArray, opts)
 }
